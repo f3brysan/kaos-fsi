1e72c61498f01b81eb7a8d73fe5af92e5ecc7a61
f3brysan <febrysan1995@gmail.com>
2023-05-06 23:51:45 +0700

    update main template
    update main template

--- resources/views/layouts/mainmenu.blade.php
+++ resources/views/layouts/mainmenu.blade.php
@@ -1,7 +1,7 @@
 <div class="main-menu menu-fixed menu-light menu-accordion menu-shadow"data-scroll-to-active="true">
     <div class="navbar-header">
         <ul class="nav navbar-nav flex-row">
-            <li class="nav-item mr-auto"><a class="navbar-brand"href="/html/ltr/vertical-menu-template/index.html">
+            <li class="nav-item mr-auto"><a class="navbar-brand"href="{{ url::to('') }}">
                     <div class="brand-logo"><img class="logo"src="/app-assets/images/logo/apple-touch-icon.png" /></div>
                     <h2 class="brand-text mb-0" style="color: black">FSI</h2>
                 </a></li>
